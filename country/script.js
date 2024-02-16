9f07ad4f844972f6e11e23856794614cbce34042
Reginaldo0Jr <juniorsantos178@hotmail.com>
2024-02-16 13:58:16 -0300

    testeBarraPesquisa

--- country/script.js
+++ country/script.js
@@ -10,20 +10,20 @@
 // });
 
 function list() {
-    fetch("https://restcountries.com/v3.1/all")
-        .then(json => json.json())
-        .then(countries => {
-            let row = document.querySelector("#row");
-            let countriesHtml = "";
-            for (let index = 0; index < countries.length; index++) {
-                const country = countries[index];
-                //nome: country.translation.por common
-                //imagem: country.flags.png
-                //alt: country.flags.alt
-                //flag: country.flag
-                //região country.region
+  fetch("https://restcountries.com/v3.1/all")
+    .then(json => json.json())
+    .then(countries => {
+      let row = document.querySelector("#row");
+      let countriesHtml = "";
+      for (let index = 0; index < countries.length; index++) {
+        const country = countries[index];
+        //nome: country.translation.por common
+        //imagem: country.flags.png
+        //alt: country.flags.alt
+        //flag: country.flag
+        //região country.region
 
-                countriesHtml += `
+        countriesHtml += `
                 <div class="col-4 mt-3">
                   <div class="card" style="width: 18rem;">
                     <img src="${country.flags.png}" class="card-img-top" alt="${country.flags.alt}">
@@ -34,10 +34,40 @@ function list() {
                   </div>
                 </div>`
 
-                
-            }
-            row.innerHTML = countriesHtml;
-        })
+
+      }
+      row.innerHTML = countriesHtml;
+    })
+
+
 }
 
+
+
 list();
+
+function search(params) {
+  fetch("https://restcountries.com/v3.1/all")
+  .then(res =>json())
+  .then((json) =>{
+    let ul = document.querySelector("row")
+    json.array.forEach((country) => {
+      let li = document.createElement("li")
+      li.innerHTML = `
+      <div class="col-4 mt-3">
+      <div class="card" style="width: 18rem;">
+        <img src="${country.flags.png}" class="card-img-top" alt="${country.flags.alt}">
+          <div class="card-body">
+            <h5 class="card-title">${country.translations.por.common}</h5>
+            <p class="card-text"> Região: ${country.region} </p>
+          </div>
+      </div>
+    </div>
+      `
+      ul.appendChild(li);
+    });
+  })
+}
+
+search();
+
